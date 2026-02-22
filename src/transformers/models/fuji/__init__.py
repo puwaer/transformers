b214680db42c49b2faa1b2100076041c5cba91f9
puwaer <y_50uki@outlook.jp>
2026-02-23 01:11:09 +0900

    fujiforcausalLM

--- src/transformers/models/fuji/__init__.py
+++ src/transformers/models/fuji/__init__.py
@@ -13,15 +13,49 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import _LazyModule
-from ...utils.import_utils import define_import_structure
+from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available
+
+
+_import_structure = {
+    "configuration_fuji": ["FujiConfig"],
+}
+
+try:
+    if not is_torch_available():
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    pass
+else:
+    _import_structure["modeling_fuji"] = [
+        "FujiForCausalLM",
+        "FujiModel",
+        "FujiPreTrainedModel",
+        "FujiForSequenceClassification",
+        "FujiForTokenClassification",
+        "FujiForQuestionAnswering",
+    ]
 
 
 if TYPE_CHECKING:
-    from .configuration_fuji import *
-    from .modeling_fuji import *
+    from .configuration_fuji import FujiConfig
+
+    try:
+        if not is_torch_available():
+            raise OptionalDependencyNotAvailable()
+    except OptionalDependencyNotAvailable:
+        pass
+    else:
+        from .modeling_fuji import (
+            FujiForCausalLM,
+            FujiModel,
+            FujiPreTrainedModel,
+            FujiForSequenceClassification,
+            FujiForTokenClassification,
+            FujiForQuestionAnswering,
+        )
+
+
 else:
     import sys
 
-    _file = globals()["__file__"]
-    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
+    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
